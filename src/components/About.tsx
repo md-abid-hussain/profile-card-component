e30b45a15646e9575eb266bded54f62fa4290ad0
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-11 17:03:54 +0530

    change color of text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,20 +6,20 @@ const About = (prop: any) => {
     console.log(name)
     return (
         <section className="text-center mt-7 ">
-            <h1 className="font-bold text-black">{name} <span className="font-normal text-[#969696]">{age}</span></h1>
+            <h1 className="font-bold text-[#2d3248]">{name} <span className="font-normal text-[#969696]">{age}</span></h1>
             <p className="text-base mt-[14px] mb-[26px]">{location}</p>
             <div className="flex border-t-2 py-7 ">
                 <section className="flex-1 flex flex-col-reverse">
                     <h2 className="text-sm">Followers</h2>
-                    <p className="font-bold text-black">{followers}</p>
+                    <p className="font-bold text-[#2d3248]">{followers}</p>
                 </section>
                 <section className="flex-1 flex flex-col-reverse">
                     <h2 className="text-sm">Likes</h2>
-                    <p className="font-bold text-black">{likes}</p>
+                    <p className="font-bold text-[#2d3248]">{likes}</p>
                 </section>
                 <section className="flex-1 flex flex-col-reverse">
                     <h2 className="text-sm">Photos</h2>
-                    <p className="font-bold text-black">{photos}</p>
+                    <p className="font-bold text-[#2d3248]">{photos}</p>
                 </section>
             </div>
         </section>
