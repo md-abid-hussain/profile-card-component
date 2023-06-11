2393f7a18ec587ba7bb4abb3b7865dfcbd65dedc
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-11 15:33:48 +0530

    added data about the profile

---
+++ src/assets/images/data/data.tsx
@@ -0,0 +1,12 @@
+const data = [
+    {
+        name: "Victor Crest",
+        age: 26,
+        location: "London",
+        followers: "80K",
+        likes: "803K",
+        photos: "1.4K"
+    }
+]
+
+export default data;
