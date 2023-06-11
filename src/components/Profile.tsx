03ee15835f17683cfdf6a6327e4b7fa68c4d0faf
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-11 16:36:11 +0530

    imported abput and data in Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,17 @@
 import pattern from '../assets/images/bg-pattern-card.svg'
 import profileImage from '../assets/images/image-victor.jpg'
+import data from '../assets/data/data'
+import About from './About'
+
 const Profile = () => {
     return (
-        <div>
-            <div className="h-[140px] min-w-[350px]" style={{ backgroundImage: `url(${pattern})` }}></div>
+        <div className="shadow-md rounded-lg bg-white">
+            <div className="h-[140px] min-w-[350px] rounded-t-lg" style={{ backgroundImage: `url(${pattern})` }}></div>
             <section>
-                <figure>
-                    <img src={profileImage} alt="profile-image" className='mx-auto rounded-full -translate-y-1/2 border-[4px]' />
+                <figure className=' -mt-[52px] '>
+                    <img src={profileImage} alt="profile-image" className='mx-auto rounded-full border-[4px] border-white' />
                 </figure>
+                <About data={data} />
             </section>
         </div>
     )
