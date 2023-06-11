a424765087b40dc58d270be16f8b6738fa17c932
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-11 15:35:45 +0530

    added header background and image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,15 @@
-
+import pattern from '../assets/images/bg-pattern-card.svg'
+import profileImage from '../assets/images/image-victor.jpg'
 const Profile = () => {
     return (
-        <div className="h-[140px] bg-slate-950"></div>
+        <div>
+            <div className="h-[140px] min-w-[350px]" style={{ backgroundImage: `url(${pattern})` }}></div>
+            <section>
+                <figure>
+                    <img src={profileImage} alt="profile-image" className='mx-auto rounded-full -translate-y-1/2 border-[4px]' />
+                </figure>
+            </section>
+        </div>
     )
 }
 
